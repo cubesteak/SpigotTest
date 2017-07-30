f3bb1e9a3d44e09e082a36bbf5e1699b586c6bf1
cubesteak <cubesteak@cubesteak.net>
2017-07-29 23:53:08 -0500

    Initial Commit

---
+++ src/net/cubesteak/mcsp/SpigotTest/Main.java
@@ -0,0 +1,15 @@
+package net.cubesteak.mcsp.SpigotTest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable(){
+        //Fired when the server enables the plugin
+    }
+
+    @Override
+    public void onDisable(){
+        //Fired when the server stops and disables all plugins
+    }
+}
